55122377cff820debcc4da0457bace5d18a3ff19
getill <98155180+getill@users.noreply.github.com>
2023-01-31 13:32:34 +0100

    Added Bootswap + base html is now extended

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    // Configuration de la route home (celle de base)
+    // Configuration de la route home (celle de base). La méthode get permet de sécuriser le site (Un tout petit peu sécurisé mais c'est un début)
     #[Route('/', 'home.index', methods: ['GET'])]
 
     // Ici on fait une fonction qui va rendre la template de la page home
     public function index(): Response
     {
+        // render vient de abstract controller et nous permet d'afficher le twig
         return $this->render('home.html.twig');
     }
 }
