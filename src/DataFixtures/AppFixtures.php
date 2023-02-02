f330f2e1460505ac3fd83aeae8ffdbc94053cd7e
getill <98155180+getill@users.noreply.github.com>
2023-02-02 13:14:28 +0100

    🗃Recipes fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -13,23 +14,45 @@ class AppFixtures extends Fixture
     /**
      * @var Generator
      */
-        private Generator $faker;
+    private Generator $faker;
 
-        public function __construct()
+    public function __construct()
     {
-        $this-> faker = Factory::create('fr_FR');
+        $this->faker = Factory::create('fr_FR');
     }
     public function load(ObjectManager $manager): void
     {
 
-        // Crée une liste de 50 ingrédients avec des prix random. On l'injecte dans la base de donnée avec php bin/console doctrine:fixtures:load
-        for ($i= 1; $i < 50; $i++) {
+        // Ingrédients
+        // Crée une liste de 50 ingrédients avec des prix random. On l'injecte dans la base de donnée avec php bin/console doctrine:fixtures:load (d:f:l)
+        $ingredients = [];
+        for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word()) // Faker va générer des mots aléatoirement
-                       ->setPrice(mt_rand(0, 100));
+                ->setPrice(mt_rand(0, 100));
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
 
+        // Recipes
+        // Crée une liste de 25 recettes avec des nom/temps/nbpeople/difficulté/description/prix/favori random. On l'injecte dans la base de donnée avec php bin/console doctrine:fixtures:load (d:f:l)
+        for ($j = 0; $j < 25; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                ->setDescription($this->faker->text(300))
+                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
+
+
+            for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+            }
+
+            $manager->persist($recipe);
+        }
 
         $manager->flush();
     }
