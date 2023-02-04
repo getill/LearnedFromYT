665137a07885f02991fcd1ba2aec1652fa0ec718
getill <98155180+getill@users.noreply.github.com>
2023-02-04 21:00:27 +0100

    🚧 User registration and login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     #[Assert\NotBlank()]
-    private ?string $password = null;
+    private ?string $password = 'password';
 
 
     #[ORM\Column]
